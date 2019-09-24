5f87e21dddd02f540b02cd708e4a7f7cc438cf65
Eugenio Batch <eugeniobatch@MacBook-Pro-de-Eugenio-3.local>
2019-09-23 22:56:34 -0300

    Elimina objeto scanner para usar los args de la consola, recambio del codigo

--- fibo/src/main/java/ar/edu/untref/dyasc/Fibonacci.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Fibonacci.java
@@ -2,13 +2,27 @@ package ar.edu.untref.dyasc;
 
 public class Fibonacci {
 	
-	public int secuenciaFibonacciDe(int n) {
-		if (n <= 1) {
-			return n;
-		}else {
-			return secuenciaFibonacciDe(n-1) + secuenciaFibonacciDe(n-2);
-		}
-	}
+    public int[] desarrollarSecuencia(int cantidadDeTerminos) {
+        int[] secuencia = new int[cantidadDeTerminos];
+        if (cantidadDeTerminos == 1) {
+            secuencia[0] = 0;
+        } else if (cantidadDeTerminos > 1) {
+            secuencia[0] = 0;
+            secuencia[1] = 1;
+        }
+        for (int i = 2; i < secuencia.length; i++) {
+            secuencia[i] = secuencia[i - 1] + secuencia[i - 2];
+        }
+        return secuencia;
+    }
+    
+    public String imprimirSecuencia(int[] serie) {
+        String salida = "fibo<" + serie.length + ">:";
+        for (int i = 0; i < serie.length; i++) {
+            salida += " " + serie[i];
+        }
+        return salida;
+    }
 }
 
 
--- fibo/src/main/java/ar/edu/untref/dyasc/Program.java
+++ fibo/src/main/java/ar/edu/untref/dyasc/Program.java
@@ -1,18 +1,14 @@
 package ar.edu.untref.dyasc;
-import java.util.Scanner;
 
 public class Program {
 		
 	
 	public static void main(String args[]) {
 		Fibonacci fibo = new Fibonacci();
-		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		s.close();
-		System.out.print("fibo<"+n+">: ");
+		int cantidadDeTerminos = Integer.parseInt(args[0]);
+		int secuencia[] = fibo.desarrollarSecuencia(cantidadDeTerminos);
 		
-		for(int i = 1; i<= n; i++) {
-			System.out.print(fibo.secuenciaFibonacciDe(i) + " ");
-		}
+		System.out.println(fibo.imprimirSecuencia(secuencia));
+		
 	}
 }
