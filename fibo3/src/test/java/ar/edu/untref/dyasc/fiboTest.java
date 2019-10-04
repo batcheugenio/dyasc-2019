ff01131ade9d1bd7cec68d2db2e787b38712d80a
Eugenio Batch <eugeniobatch@MacBook-Pro-de-Eugenio-3.local>
2019-10-03 22:56:23 -0300

    Agrego test de fibo2 para fibo3

---
+++ fibo3/src/test/java/ar/edu/untref/dyasc/fiboTest.java
@@ -0,0 +1,15 @@
+package ar.edu.untref.dyasc;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class fiboTest {
+
+    @Test
+    public void imprimirSecuenciaFiboTest() {
+        Fibonacci miFibo = new Fibonacci();
+        int [] secuencia = miFibo.secuenciaFibonacciDe(5);
+        assertEquals(miFibo.imprimirSecuencia(secuencia, 'h', 'd'), "fibo<5>: 0 1 1 2 3");
+    }
+}
