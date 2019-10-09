02b7ba2309712b198dc2946354bf6e4a6d17272b
Eugenio Batch <eugeniobatch@MacBook-Pro-de-Eugenio-3.local>
2019-10-08 23:43:14 -0300

    agrego y modifico nombre tests en lector de parametros

--- fibo3/src/test/java/ar/edu/untref/dyasc/LectorDeParametrosTest.java
+++ fibo3/src/test/java/ar/edu/untref/dyasc/LectorDeParametrosTest.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 public class LectorDeParametrosTest {
 
     @Test
-    public void leerParametrosTest() throws OpcionNoValidaException {
+    public void creaLectorConOpcionHorizontalYDerecho() throws OpcionNoValidaException {
         LectorDeParametros miLector = new LectorDeParametros();
         String[] parametros = {"-o=hd", "5"};
         miLector.leerParametros(parametros);
         Assert.assertEquals(miLector.getOpcion(), "hd");
+    }
+
+    @Test
+    public void creaLectorConOpcionVerticalEInverso() throws OpcionNoValidaException {
+        LectorDeParametros miLector = new LectorDeParametros();
+        String[] parametros = {"-o=vi", "5"};
+        miLector.leerParametros(parametros);
+        Assert.assertEquals(miLector.getOpcion(), "vi");
     }
 }
