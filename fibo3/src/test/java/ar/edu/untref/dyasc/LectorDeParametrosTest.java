2406b4357649e3fd33b83784bfc78c84bebf55d2
Eugenio Batch <eugeniobatch@MacBook-Pro-de-Eugenio-3.local>
2019-10-09 00:24:22 -0300

    Agrega prueba de excepcion

--- fibo3/src/test/java/ar/edu/untref/dyasc/LectorDeParametrosTest.java
+++ fibo3/src/test/java/ar/edu/untref/dyasc/LectorDeParametrosTest.java
@@ -1,5 +1,6 @@
 package ar.edu.untref.dyasc;
 
+import cucumber.api.java.es.Y;
 import jdk.nashorn.internal.runtime.regexp.joni.constants.OPCode;
 import org.junit.Assert;
 import org.junit.Test;
@@ -29,5 +30,23 @@ public class LectorDeParametrosTest {
 
         miLector.leerParametros(parametros);
         Assert.assertEquals(miLector.getArchivo(), "archivoDePrueba.txt");
+    }
+
+    @Test
+    public void crearLectorConOpcionArchivoYModoSumatoria() throws OpcionNoValidaException {
+        LectorDeParametros miLector = new LectorDeParametros();
+        String[] parametros = {"-o=vi", "-f=archivoDePrueba.txt", "-m=s"};
+
+        miLector.leerParametros(parametros);
+        Assert.assertEquals(miLector.getArchivo(), "archivoDePrueba.txt");
+        Assert.assertEquals(miLector.getModo(), 's');
+        Assert.assertEquals(miLector.getOpcion(), "vi");
+    }
+
+    @Test(expected = OpcionNoValidaException.class)
+    public void crearLectorConOpcionInvalida() throws OpcionNoValidaException {
+        LectorDeParametros miLector = new LectorDeParametros();
+        String[] parametros = {"-o=xy", "-f=archivoDePrueba.txt", "-m=s"};
+        miLector.leerParametros(parametros);
     }
 }
