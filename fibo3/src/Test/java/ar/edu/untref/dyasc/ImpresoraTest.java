57efd7efdb0740dcc6228b092e3307ba21e0e7fd
Eugenio Batch <batcheugenio@gmail.com>
2019-10-12 19:19:16 -0300

    Agrego obtenerCabeceraPorImpresoraTest

--- fibo3/src/Test/java/ar/edu/untref/dyasc/ImpresoraTest.java
+++ fibo3/src/Test/java/ar/edu/untref/dyasc/ImpresoraTest.java
@@ -6,9 +6,22 @@ import org.junit.Test;
 public class ImpresoraTest {
 
     @Test
-    public void crearImpresoraTest() {
-        Impresora miImpresora = new Impresora();
-        Assert.assertEquals(miImpresora.imprimir(), "Imprimiendo...");
+    public void obtenerSecuenciaPorImpresoraTest(){
+        Fibonacci miFibo = new Fibonacci();
+        miFibo.generarSecuenciaFibonacciDe(5);
+
+        Impresora miImpresora = new Impresora(miFibo);
+        Assert.assertEquals(miImpresora.imprimir(), "Imprimiendo Secuencia: [0, 1, 1, 2, 3]");
     }
 
+    @Test
+    public void obtenerCabeceraPorImpresoraTest(){
+        Fibonacci miFibo = new Fibonacci();
+        miFibo.generarSecuenciaFibonacciDe(5);
+
+        Impresora miImpresora = new Impresora(miFibo);
+        miImpresora.crearCabecera();
+
+        Assert.assertEquals("fibo<5>", miImpresora.getCabecera());
+    }
 }
--- fibo3/src/main/java/ar/edu/untref/dyasc/Impresora.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/Impresora.java
@@ -3,10 +3,13 @@ package ar.edu.untref.dyasc;
 import java.util.Arrays;
 
 public class Impresora {
+    private String cabecera;
     private int [] secuencia;
+    private Fibonacci fibonacci;
 
-    Impresora(Fibonacci miFibo){
-        setSecuencia(miFibo.getSecuencia());
+    Impresora(Fibonacci fibonacci){
+        setSecuencia(fibonacci.getSecuencia());
+        this.fibonacci = fibonacci;
     }
 
     public String imprimir() {
@@ -18,5 +21,19 @@ public class Impresora {
     }
     public void setSecuencia(int[] secuencia){
         this.secuencia = secuencia;
+    }
+
+    public void crearCabecera() {
+        StringBuilder cabecera = new StringBuilder();
+        cabecera.append("fibo<" + String.valueOf(this.fibonacci.getNumeroDeSecuencia()) + ">");
+        this.setCabecera(cabecera.toString());
+    }
+
+    public void setCabecera(String cabecera){
+        this.cabecera = cabecera;
+    }
+
+    public String getCabecera() {
+        return this.cabecera;
     }
 }
