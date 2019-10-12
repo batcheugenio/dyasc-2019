1680aaf6b55ae073c363990c061b72c34de76435
Eugenio Batch <batcheugenio@gmail.com>
2019-10-12 20:35:02 -0300

    Agrego tests a ImpresoraTest

--- fibo3/src/Test/java/ar/edu/untref/dyasc/ImpresoraTest.java
+++ fibo3/src/Test/java/ar/edu/untref/dyasc/ImpresoraTest.java
@@ -6,22 +6,59 @@ import org.junit.Test;
 public class ImpresoraTest {
 
     @Test
-    public void obtenerSecuenciaPorImpresoraTest(){
-        Fibonacci miFibo = new Fibonacci();
-        miFibo.generarSecuenciaFibonacciDe(5);
-
-        Impresora miImpresora = new Impresora(miFibo);
-        Assert.assertEquals(miImpresora.imprimir(), "Imprimiendo Secuencia: [0, 1, 1, 2, 3]");
-    }
-
-    @Test
     public void obtenerCabeceraPorImpresoraTest(){
         Fibonacci miFibo = new Fibonacci();
         miFibo.generarSecuenciaFibonacciDe(5);
-
         Impresora miImpresora = new Impresora(miFibo);
         miImpresora.crearCabecera();
 
-        Assert.assertEquals("fibo<5>", miImpresora.getCabecera());
+        Assert.assertEquals("fibo<5>:", miImpresora.getCabecera());
+    }
+
+    @Test
+    public void armarYObtenerContenidoPorImpresoraTest(){
+        Fibonacci miFibo = new Fibonacci();
+        miFibo.generarSecuenciaFibonacciDe(5);
+        Impresora miImpresora = new Impresora(miFibo, "vi", "s");
+        miImpresora.crearCuerpo();
+
+        Assert.assertEquals("7", miImpresora.getCuerpo());
+    }
+
+    @Test
+    public void obtenerImpresionPorDefaultDeSecuenciaTest(){
+        Fibonacci miFibo = new Fibonacci();
+        miFibo.generarSecuenciaFibonacciDe(5);
+        Impresora miImpresora = new Impresora(miFibo);
+
+        Assert.assertEquals("fibo<5>: 0 1 1 2 3", miImpresora.imprimir());
+    }
+
+    @Test
+    public void obtenerImpresionModoSumatoriaTest(){
+        Fibonacci miFibo = new Fibonacci();
+        miFibo.generarSecuenciaFibonacciDe(5);
+        Impresora miImpresora = new Impresora(miFibo, "hd", "s");
+
+        Assert.assertEquals("fibo<5>s: 7", miImpresora.imprimir());
+
+    }
+
+    @Test
+    public void obtenerImpresionOpcionInversaTest(){
+        Fibonacci miFibo = new Fibonacci();
+        miFibo.generarSecuenciaFibonacciDe(5);
+        Impresora miImpresora = new Impresora(miFibo, "hi", "l");
+
+        Assert.assertEquals("fibo<5>: 3 2 1 1 0", miImpresora.imprimir());
+    }
+
+    @Test
+    public void obtenerImpresionOpcionInversaYVerticalTest(){
+        Fibonacci miFibo = new Fibonacci();
+        miFibo.generarSecuenciaFibonacciDe(5);
+        Impresora miImpresora = new Impresora(miFibo, "vi", "l");
+
+        Assert.assertEquals("fibo<5>:\n3\n2\n1\n1\n0", miImpresora.imprimir());
     }
 }
--- fibo3/src/main/java/ar/edu/untref/dyasc/Impresora.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/Impresora.java
@@ -1,24 +1,42 @@
 package ar.edu.untref.dyasc;
 
+import javafx.beans.binding.StringBinding;
+
 import java.util.Arrays;
 
 public class Impresora {
     private String cabecera;
     private int [] secuencia;
+    private String cuerpo;
+    private String salida;
+    private String modo;
+    private String opcion;
     private Fibonacci fibonacci;
 
     Impresora(Fibonacci fibonacci){
         setSecuencia(fibonacci.getSecuencia());
         this.fibonacci = fibonacci;
+        this.modo = "l";
+        this.opcion="hd";
     }
 
+    Impresora(Fibonacci fibonacci, String opcion, String modo){
+        this(fibonacci);
+        this.opcion = opcion;
+        this.modo = modo;
+    }
+
+
     public String imprimir() {
-        return "Imprimiendo Secuencia: " + Arrays.toString(getSecuencia());
+        this.crearCabecera();
+        this.crearCuerpo();
+        return this.getCabecera() + this.getCuerpo();
     }
 
     public int[] getSecuencia(){
         return this.secuencia;
     }
+
     public void setSecuencia(int[] secuencia){
         this.secuencia = secuencia;
     }
@@ -26,6 +44,11 @@ public class Impresora {
     public void crearCabecera() {
         StringBuilder cabecera = new StringBuilder();
         cabecera.append("fibo<" + String.valueOf(this.fibonacci.getNumeroDeSecuencia()) + ">");
+        if(this.modo == "s"){
+            cabecera.append("s");
+        }
+        cabecera.append(":");
+
         this.setCabecera(cabecera.toString());
     }
 
@@ -35,5 +58,52 @@ public class Impresora {
 
     public String getCabecera() {
         return this.cabecera;
+    }
+
+    public void setCuerpo(String cuerpo){
+        this.cuerpo = cuerpo;
+    }
+
+    public String getCuerpo(){
+        return this.cuerpo;
+    }
+
+    public void crearCuerpo() {
+
+        StringBuilder cuerpo = new StringBuilder();
+        if(this.modo == "s"){
+            cuerpo = this.imprimirSumatoria(cuerpo);
+        } else {
+            cuerpo = this.imprimirSecuencia(cuerpo);
+        }
+        this.setCuerpo(cuerpo.toString());
+    }
+
+    private StringBuilder imprimirSumatoria(StringBuilder cuerpo){
+        if(this.modo == "s"){
+            cuerpo.append(" "+Integer.toString(fibonacci.getSumatoria()));
+        }
+        return cuerpo;
+    }
+
+    private StringBuilder imprimirSecuencia(StringBuilder cuerpo) {
+        if (this.opcion == "hi"){
+            for(int i = secuencia.length -1; i>=0; i--) {
+                cuerpo.append(" " + Integer.toString(secuencia[i]));
+            }
+        } else if (this.opcion == "vd"){
+            for(int i=0; i<secuencia.length; i++){
+                cuerpo.append("\n" + Integer.toString(secuencia[i]));
+            }
+        } else if(this.opcion == "vi"){
+            for(int i=secuencia.length -1; i>=0; i--) {
+                cuerpo.append("\n" + Integer.toString(secuencia[i]));
+            }
+        } else {
+            for (int i=0; i<secuencia.length; i++){
+                cuerpo.append(" " + Integer.toString(secuencia[i]));
+            }
+        }
+        return cuerpo;
     }
 }
