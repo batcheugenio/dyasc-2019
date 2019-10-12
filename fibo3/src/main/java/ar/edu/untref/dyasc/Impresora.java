8eb27eb3f3a9b487a2fb751c7a54a81df065659a
Eugenio Batch <eugeniobatch@MacBook-Pro-de-Eugenio-3.local>
2019-10-12 18:15:25 -0300

    agrego obtenerSecuenciaPorImpresoraTest

--- fibo3/src/main/java/ar/edu/untref/dyasc/Impresora.java
+++ fibo3/src/main/java/ar/edu/untref/dyasc/Impresora.java
@@ -3,8 +3,20 @@ package ar.edu.untref.dyasc;
 import java.util.Arrays;
 
 public class Impresora {
+    private int [] secuencia;
+
+    Impresora(Fibonacci miFibo){
+        setSecuencia(miFibo.getSecuencia());
+    }
 
     public String imprimir() {
-        return "Imprimiendo...";
+        return "Imprimiendo Secuencia: " + Arrays.toString(getSecuencia());
+    }
+
+    public int[] getSecuencia(){
+        return this.secuencia;
+    }
+    public void setSecuencia(int[] secuencia){
+        this.secuencia = secuencia;
     }
 }
